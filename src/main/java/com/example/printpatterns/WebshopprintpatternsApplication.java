8cef1799badcdf4b4ecd7dcd33a2b26538cd59c8
MRijvers <mjlprijv@avans.nl>
2018-07-05 11:12:05 +0200

    1e commit

---
+++ src/main/java/com/example/printpatterns/WebshopprintpatternsApplication.java
@@ -0,0 +1,12 @@
+package com.example.printpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopprintpatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopprintpatternsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/printpatterns/domain/Product.java
@@ -0,0 +1,4 @@
+package com.example.printpatterns.domain;
+
+public class Product {
+}
