1efd18b0099ae694f72a0f8683e68c9fac936682
MRijvers <mjlprijv@avans.nl>
2018-07-05 11:40:38 +0200

    productrepo

--- src/main/java/com/example/printpatterns/domain/Product.java
+++ src/main/java/com/example/printpatterns/domain/Product.java
@@ -1,4 +1,17 @@
 package com.example.printpatterns.domain;
 
-public class Product {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product
+{
+    private long productId;
+    private String productName;
+    private String description;
+    private double price;
+    private int stock;
+    private String type;
+    private String colour;
 }
